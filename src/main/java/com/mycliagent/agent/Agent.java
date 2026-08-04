0af8df8d6212c3e91686fbf4267deb45e2170619
Your Name <153814332+Rishi-Mishima@users.noreply.github.com>
2026-08-04 19:01:03 +0200

    feat: ReAct core loop in Agent

--- src/main/java/com/mycliagent/agent/Agent.java
+++ src/main/java/com/mycliagent/agent/Agent.java
@@ -44,4 +44,55 @@ public class Agent {
         // 创建AGENT后, 聊天记录并不是空的 而是[System Prompt]
         conversationHistory.add(LlmClient.Message.system(SYSTEM_PROMPT));
     }
+
+    public String run(String userInput) {
+        // 添加用户输入 - 加入聊天历史
+        conversationHistory.add(Message.user(userInput));
+
+        // 初始化迭代次数
+        int iteration = 0;
+        //开始Agent循环, 小于Max
+        while (iteration < MAX_ITERATIONS) {
+            iteration++;
+
+            // 调用 LLM: 发送当前完整聊天记录, 以及可以使用的工具定义
+            ChatResponse response = llmClient.chat(
+                    conversationHistory,
+                    toolRegistry.getToolDefinitions()
+            );
+
+            // 如果有工具调用 - 检查大模型返回的响应中，是否包含工具调用请求。
+            if (response.hasToolCalls()) {
+                // 记录助手消息
+                conversationHistory.add(
+                        Message.assistant(response.content(), response.toolCalls())
+                );
+
+                // 遍历执行每个工具调用 - 因为一条AI恢复可能包含多个工具调用
+                for (ToolCall toolCall : response.toolCalls()) {
+                    //执行工具
+                    String result = toolRegistry.executeTool(
+                            toolCall.function().name(),
+                            toolCall.function().arguments()
+                    );
+
+                    // 记录工具结果
+                    conversationHistory.add(
+                            // toolCall.id()表示工具调用的唯一ID
+                            Message.tool(toolCall.id(), result)
+                    );
+                }
+                // 继续循环，让 LLM 根据结果继续思考
+                continue;
+            } else {
+                // 没有工具调用，任务完成
+                conversationHistory.add(
+                        Message.assistant(response.content())
+                );
+                return response.content();
+            }
+        }
+
+        return "达到最大迭代次数限制";
+    }
 }
